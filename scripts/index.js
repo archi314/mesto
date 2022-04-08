1bb74747d9d96ab47af468e3df4d77f173556f28
Artem Stukalov <artem.s95@mail.ru>
2022-04-08 23:18:31 +0300

    Проектная работа Mesto вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,29 @@
 let editButton = document.querySelector('.profile__edit-button');
 let modalWindow = document.querySelector('.popup');
 let modalCloseBtn = modalWindow.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form'); 
+let nameInput = document.querySelector('.popup__user-name'); 
+let jobInput = document.querySelector('.popup__user-description'); 
+let profileName = document.querySelector('.profile__title'); 
+let profileStatus = document.querySelector('.profile__subtitle'); 
 
-function toggleModalWindow() {
+
+function toggleModalWindow() { 
   modalWindow.classList.toggle('popup_opened');
-  nameInput.textContent = profileName.value;
-  jobInput.textContent = profileStatus.value;
+  if (popup.classList.contains('popup_opened')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileStatus.textContent;
+  }
 }
 
 editButton.addEventListener('click', toggleModalWindow);
 modalCloseBtn.addEventListener('click', toggleModalWindow);
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__user-name');
-let jobInput = document.querySelector('.popup__user-description');
-let profileName = document.querySelector('.profile__title');
-let profileStatus = document.querySelector('.profile__subtitle');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    profileName.textContent = nameInput.value;
-    profileStatus.textContent = jobInput.value;
-    toggleModalWindow();  
-}
+function formSubmitHandler (evt) { 
+  evt.preventDefault();  
+  profileName.textContent = nameInput.value; 
+  profileStatus.textContent = jobInput.value; 
+  toggleModalWindow();   
+} 
 
 formElement.addEventListener('submit', formSubmitHandler);
